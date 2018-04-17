35323d5d32552bb34179a42b6241d96ac4bdbf6b
Clint Shryock <clint@ctshryock.com>
2018-04-17 11:57:22 -0500

    add some cli args

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -20,21 +20,121 @@ import (
 	"github.com/vbauerster/mpb/decor"
 )
 
-func main() {
-	// TODO: accept cli argument
-	rInput := reposForOrgInput{
-		Name: "terraform-providers",
+// searchFilter is used to provide options when searching repos for an org.
+type searchFilter struct {
+	Organizations []string
+	Repositories  []string
+	Limit         int
+	Concurrency   int
+	ShowErrs      bool
+}
+
+func (s searchFilter) String() string {
+	var orgs, repos string
+	if len(s.Organizations) > 0 {
+		if len(s.Organizations) > 3 {
+			orgs = "many organizations"
+		} else {
+			orgs = fmt.Sprintf("(%s)", strings.Join(s.Organizations, ", "))
+		}
 	}
-	repos, err := reposForOrg(&rInput)
+	if len(s.Repositories) > 0 {
+		if len(s.Repositories) > 3 {
+			orgs = "many repositories"
+		} else {
+			orgs = fmt.Sprintf("(%s)", strings.Join(s.Repositories, ", "))
+		}
+		return orgs + " and " + repos
+	}
+	return orgs
+}
+
+func main() {
+	helpText := `
+Usage: deptrack [options] 
+
+  Gather dependencies from Go projects across your projects on GitHub.
+
+Options:
+  -o, --organizations 	Crawl all repositories an organization. Comma seperated list. Default "terraform-providers"
+  -r, --repositories 	Crawl only these repositories (comman seperated list). Must be full name "org/repo"
+  -l, --limit 		Limit number or repos to crawl. Default is to report on all repositories listed, or all found in the given organization
+  -c, --concurrency 	Concurrency devel; default 10
+  -e, --errors 		Flag to list repos that returned an error. Default false
+  -h, --help 		Print this message. But you knew this by now
+`
+
+	// TODO: accept cli argument
+	filter := searchFilter{
+		Organizations: []string{"terraform-providers"},
+		Concurrency:   10,
+	}
+
+	args := os.Args[1:]
+	if len(args) > 0 {
+		for i, a := range args {
+			if a == "-h" || a == "-help" {
+				fmt.Println(helpText)
+				os.Exit(0)
+			}
+			if a == "-o" || a == "-organizations" {
+				filter.Organizations = strings.Split(args[i+1], ",")
+			}
+			if a == "-r" || a == "-repositories" {
+				filter.Repositories = strings.Split(args[i+1], ",")
+			}
+			if a == "-l" || a == "-limit" {
+				i, err := strconv.Atoi(args[i+1])
+				if err != nil {
+					fmt.Printf("[WARN] Error parsing limit value: %s", err)
+					continue
+				}
+				filter.Limit = i
+			}
+			if a == "-c" || a == "-concurrency" {
+				i, err := strconv.Atoi(args[i+1])
+				if err != nil {
+					fmt.Printf("[WARN] Error parsing concurrency value: %s", err)
+					continue
+				}
+				filter.Concurrency = i
+			}
+
+			if a == "-e" || a == "-errors" {
+				filter.ShowErrs = true
+			}
+		}
+	}
+
+	if len(filter.Organizations) == 0 && len(filter.Repositories) == 0 {
+		fmt.Println("No organizations or repositories provided")
+		os.Exit(1)
+	}
+
+	repos, err := reposForOrg(&filter)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 		os.Exit(1)
 	}
 
+	if len(repos) == 0 {
+		fmt.Println("No repositories found")
+		os.Exit(0)
+	}
+
+	if filter.Limit > 0 {
+		fmt.Println("Limiting to (%d) repositories", filter.Limit)
+		repos = repos[:filter.Limit]
+	}
+
+	if len(repos) < filter.Concurrency {
+		filter.Concurrency = len(repos)
+	}
+
 	repoChan := make(chan *repoDepResult, len(repos))
 	resultsChan := make(chan *repoDepResult, len(repos))
 
-	concurrency := 3
+	concurrency := filter.Concurrency
 	var wg sync.WaitGroup
 	wg.Add(concurrency)
 
@@ -61,19 +161,24 @@ func main() {
 	go func() {
 		for _, r := range repos {
 			repoChan <- &repoDepResult{
-				Org:  rInput.Name,
-				Name: *r.Name,
+				FullName: *r.FullName,
+				Name:     *r.Name,
 			}
 		}
 		close(repoChan)
 	}()
 
-	fmt.Printf("Gathering dependencies for (%s)...\n", rInput.Name)
+	fmt.Printf("Gathering dependencies for %s...\n", filter)
 	wg.Wait()
 	p.Wait()
 	close(resultsChan)
 	var results []*repoDepResult
+	var errd []*repoDepResult
 	for r := range resultsChan {
+		if r.err != nil {
+			errd = append(errd, r)
+			continue
+		}
 		results = append(results, r)
 	}
 	bar.Complete()
@@ -88,47 +193,55 @@ func main() {
 		}
 	}
 
-	// save to file
-	fmt.Println("Saving to 'dep_result.csv'...")
-	f, err := os.OpenFile("dep_result.csv", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Printf("Error saving file: %s", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	f.WriteString("Package,Revision,Version,Exact Version,Providers\n")
-	var keys []string
-	for k := range depMap {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		repos := depMap[k]
-		// fmt.Printf("%s,%s\n", k, strings.Join(repos, ","))
-		parts := make([]string, 4, 4)
-		d := strings.Split(k, "::")
-		// key format:
-		// path::revision::version::version_exact
-		// where version and version exact may not exist
-		for i, s := range d {
-			parts[i] = s
+	if len(results) > 0 {
+		// save to file
+		fmt.Println("Saving to 'dep_result.csv'...")
+		f, err := os.OpenFile("dep_result.csv", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Printf("Error saving file: %s", err)
+			os.Exit(1)
 		}
-		sort.Strings(repos)
-		f.WriteString(fmt.Sprintf("%s,%s\n", strings.Join(parts, ","), strings.Join(repos, ",")))
+		defer f.Close()
+		f.WriteString("Package,Revision,Version,Exact Version,Providers\n")
+		var keys []string
+		for k := range depMap {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			repos := depMap[k]
+			// fmt.Printf("%s,%s\n", k, strings.Join(repos, ","))
+			parts := make([]string, 4, 4)
+			d := strings.Split(k, "::")
+			// key format:
+			// path::revision::version::version_exact
+			// where version and version exact may not exist
+			for i, s := range d {
+				parts[i] = s
+			}
+			sort.Strings(repos)
+			f.WriteString(fmt.Sprintf("%s,%s\n", strings.Join(parts, ","), strings.Join(repos, ",")))
+		}
 	}
+
+	// show a summary
+	fmt.Printf("\n\nResults found: %d\nError count: %d\n", len(results), len(errd))
+	if len(errd) > 0 && filter.ShowErrs {
+		fmt.Println("Repos with error status:")
+		for _, r := range errd {
+			fmt.Printf("- %s err: %s\n", r.FullName, r.err)
+		}
+	}
+
 	fmt.Println("Done!")
 }
 
 // repoDepResult stores information on a dependency
 type repoDepResult struct {
-	Org  string
-	Name string
-	Deps *vendorDeps
-	err  error
-}
-
-func (r *repoDepResult) FullName() string {
-	return fmt.Sprintf("%s/%s", r.Org, r.Name)
+	FullName string
+	Name     string
+	Deps     *vendorDeps
+	err      error
 }
 
 func (r *repoDepResult) RepoName() string {
@@ -167,13 +280,11 @@ func fetchVendor(wg *sync.WaitGroup, bar *mpb.Bar, repoChan <-chan *repoDepResul
 	for r := range repoChan {
 		// raw file url
 		// https://raw.githubusercontent.com/terraform-providers/terraform-provider-aws/master/vendor/vendor.json
-		url := fmt.Sprintf("https://raw.githubusercontent.com/%s/master/vendor/vendor.json", r.FullName())
-		// log.Printf("\nurl: %s\n", url)
+		url := fmt.Sprintf("https://raw.githubusercontent.com/%s/master/vendor/vendor.json", r.FullName)
 
 		// Submit the request
 		resp, err := client.Get(url)
 		if err != nil {
-			log.Printf("\nFailed to GET (%s): %s", url, err)
 			r.err = fmt.Errorf("railed to GET (%s): %s", url, err)
 			bar.Increment()
 			resultsChan <- r
@@ -182,7 +293,7 @@ func fetchVendor(wg *sync.WaitGroup, bar *mpb.Bar, repoChan <-chan *repoDepResul
 
 		// Check the response
 		if resp.StatusCode != http.StatusOK {
-			r.err = fmt.Errorf("NotOK status for (%s): %s", r.FullName(), resp.Status)
+			r.err = fmt.Errorf("%s", resp.Status)
 			bar.Increment()
 			resultsChan <- r
 			continue
@@ -191,7 +302,7 @@ func fetchVendor(wg *sync.WaitGroup, bar *mpb.Bar, repoChan <-chan *repoDepResul
 		var deps vendorDeps
 		if err := json.NewDecoder(resp.Body).Decode(&deps); err != nil {
 			resp.Body.Close()
-			log.Printf("\n\tfailed to parse response for (%s): %s", r.FullName(), err)
+			r.err = fmt.Errorf("\n\tfailed to parse response for (%s): %s", r.FullName, err)
 		}
 		resp.Body.Close()
 		r.Deps = &deps
@@ -202,22 +313,14 @@ func fetchVendor(wg *sync.WaitGroup, bar *mpb.Bar, repoChan <-chan *repoDepResul
 	wg.Done()
 }
 
-// reposForOrgInput is used to provide options when searching repos for an org.
-// Right now just supports name.
-type reposForOrgInput struct {
-	Name string
-}
-
-// reposForOrg returns all repos for an org. If input is nil, uses default org
+// reposForOrg returns all repos for an org. If filter is nil, uses default org
 // of Terraform Providers
-func reposForOrg(input *reposForOrgInput) ([]*github.Repository, error) {
+func reposForOrg(filter *searchFilter) ([]*github.Repository, error) {
 	key := os.Getenv("GITHUB_API_TOKEN")
 	if key == "" {
 		return nil, fmt.Errorf("missing API Token")
 	}
-	if input == nil || input.Name == "" {
-		input = &reposForOrgInput{Name: "terraform-providers"}
-	}
+
 	// refactor, this is boilerplate
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -229,19 +332,21 @@ func reposForOrg(input *reposForOrgInput) ([]*github.Repository, error) {
 
 	// get list of repositories across terraform-repositories, and add in
 	// hashicorp/terraform
-	nopt := &github.RepositoryListByOrgOptions{}
 	var repos []*github.Repository
-	for {
-		part, resp, err := client.Repositories.ListByOrg(ctx, input.Name, nopt)
+	for _, org := range filter.Organizations {
+		nopt := &github.RepositoryListByOrgOptions{}
+		for {
+			part, resp, err := client.Repositories.ListByOrg(ctx, org, nopt)
 
-		if err != nil {
-			return nil, fmt.Errorf("error listing Repositories: %s", err)
+			if err != nil {
+				return nil, fmt.Errorf("error listing Repositories: %s", err)
+			}
+			repos = append(repos, part...)
+			if resp.NextPage == 0 {
+				break
+			}
+			nopt.Page = resp.NextPage
 		}
-		repos = append(repos, part...)
-		if resp.NextPage == 0 {
-			break
-		}
-		nopt.Page = resp.NextPage
 	}
 	return repos, nil
 }
